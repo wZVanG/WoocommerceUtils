4a64cd979b7b7382718921eb89b20668f3322458
Walter Chapilliquen <wz.vang@gmail.com>
2024-06-14 19:13:00 -0500

    leg

--- woo-advanced-utils/includes/actions/wau_action_customendpoint.php
+++ woo-advanced-utils/includes/actions/wau_action_customendpoint.php
@@ -11,7 +11,6 @@ add_action('rest_api_init', function(){
 	));
 });
 
-
 function custom_get_advanced_products($request) {
     global $wpdb;
 
@@ -19,17 +18,13 @@ function custom_get_advanced_products($request) {
     $parameters = $request->get_params();
 
     // Verifica si 'skus' está presente y es una cadena
-    if (!isset($parameters['skus']) || !is_string($parameters['skus'])) {
-        return new WP_Error('invalid_skus', 'No has especificado los SKUs', array('status' => 400));
-    }
+    if (!isset($parameters['skus']) || !is_string($parameters['skus'])) return new WP_Error('invalid_skus', 'No has especificado los SKUs', array('status' => 400));
 
     // Divide la cadena en un array usando la coma como delimitador
     $skus = explode(',', $parameters['skus']);
 
     // Verifica si la división resultó en un array no vacío
-    if (!is_array($skus) || empty($skus)) {
-        return new WP_Error('invalid_skus', 'Invalid SKUs parameter', array('status' => 400));
-    }
+    if (!is_array($skus) || empty($skus)) return new WP_Error('invalid_skus', 'Invalid SKUs parameter', array('status' => 400));
 
     // Procede con la consulta a la base de datos
     $placeholders = implode(',', array_fill(0, count($skus), '%s'));
@@ -43,18 +38,16 @@ function custom_get_advanced_products($request) {
 
     $results = $wpdb->get_results($query);
 
-    if (empty($results)) {
-        return new WP_REST_Response([], 200);
-    }
+    if (empty($results)) return new WP_REST_Response([], 200);
 
-    $products = [];
+    $items = [];
     foreach ($results as $result) {
-        $products[] = [
-            'id' => $result->ID,
+        $items[] = [
+            'id' => (int) $result->ID,
             'name' => $result->post_title,
             'sku' => $result->sku,
         ];
     }
 
-    return new WP_REST_Response($products, 200);
+    return new WP_REST_Response($items, 200);
 }
